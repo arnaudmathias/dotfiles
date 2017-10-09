a888f1de91d9773ba706c4083dcde7f5dfd55c57
IndieDriver <amathias63@gmail.com>
2017-10-09 21:49:55 +0200

    update vim-plug on dot_update

--- .zsh/custom_functions.zsh
+++ .zsh/custom_functions.zsh
@@ -2,4 +2,8 @@ function dot_update() {
     cd ~/.dotfiles/;
     git pull;
     git submodule update --recursive --remote;
+    #update vim plug
+    rm -f ~/.vim/autoload/plug.vim;
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # dotfiles
 ```
-cd ~
-git clone https://github.com/IndieDriver/dotfiles.git .dotfiles --recursive
-cd .dotfiles
+git clone https://github.com/IndieDriver/dotfiles.git ~/.dotfiles --recursive
+cd ~/.dotfiles
 sh install.sh
 ```
