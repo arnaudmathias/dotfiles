83a58a5de7ef7f7ade80945ac016e4f642e72a43
indiedriver <amathias63@gmail.com>
2018-01-12 18:59:41 +0100

    pricedb updater script refactor

--- scripts/pricedb_update.py
+++ scripts/pricedb_update.py
@@ -1,20 +1,85 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+File: pricedb_update.py
+Author: indiedriver
+Email: arndmathias@gmail.com
+Github: https://github.com/indiedriver
+Description: Populate ledger .pricedb file with historical price data
+"""
 
 import os
 import json
-import gdax
+import requests
 import datetime
+import time
+import parse
 
 pricedb_path = os.environ['HOME'] + os.sep + ".pricedb"
+currencies = {'BTC', 'BCH', 'ETH', 'XRP'}
+fiat_symbol = '€'
+fiat_query = 'EUR'
+start_at = '2017/01/01' # YYYY/MM/DD
 
-client = gdax.PublicClient()
-eth_price = client.get_product_ticker(product_id='ETH-EUR')["price"]
-btc_price = client.get_product_ticker(product_id='BTC-EUR')["price"]
+def getTimestamp(datetime):
+    return str(int(time.mktime(datetime.timetuple())))
 
-pricedb_file = file(pricedb_path,"a")
-current_time = datetime.datetime.now()
+def getHistoricalPrice(symbol, fiat, timestamp = ''):
+    if timestamp:
+        payload = {'fsym': symbol, 'tsyms': fiat, 'ts': timestamp}
+    else:
+        payload = {'fsym': symbol, 'tsyms': fiat}
+    r = requests.get('https://min-api.cryptocompare.com/data/pricehistorical', params=payload);
+    data = json.loads(r.text)
+    if 'Response' in data:
+        print (data);
+    return (data[symbol][fiat_query])
 
-with open(pricedb_path, "a") as file:
-    file.write("P " + current_time.strftime("%Y/%m/%d %H:%M:%S") + " BTC " + str(btc_price) + "€\n")
-    file.write("P " + current_time.strftime("%Y/%m/%d %H:%M:%S") + " ETH " + str(eth_price) + "€\n")
+def writePriceDatabase(list):
+    with open(pricedb_path, "w") as file:
+        for entry_list in list:
+            file.write("P " + entry_list['date'].strftime("%Y/%m/%d %H:%M:%S") + " "
+                       + entry_list['symbol'] + " " + entry_list['price'] + fiat_symbol + "\n")
+
+def queryPrice(dt, cur):
+    price = getHistoricalPrice(cur, fiat_query, getTimestamp(dt));
+    info = {
+        "date": dt,
+        "symbol": cur,
+        "price":  str(price),
+    }
+    return (info)
+
+def main():
+    content = []
+    parserExp = parse.compile("P {date} {time} {symbol} {value}" + fiat_symbol);
+    if os.path.exists(pricedb_path):
+        with open(pricedb_path, "r") as file:
+            for line in file:
+                res = parserExp.parse(line);
+                dt = datetime.datetime.strptime(res['date'] + " " + res['time'], "%Y/%m/%d %H:%M:%S")
+                info = {
+                    "date": dt,
+                    "symbol": res['symbol'],
+                    "price":  str(res['value']),
+                }
+                content.append(info)
+    db_content = []
+    startDate = datetime.datetime.strptime(start_at, "%Y/%m/%d")
+    delta = datetime.datetime.now() - startDate;
+    for cur in currencies:
+        cur_list = filter(lambda c: c['symbol'] == cur, content)
+        for i in range(delta.days + 1):
+            dt = startDate + datetime.timedelta(days=i)
+            res = filter(lambda date: date['date'].date() == dt.date(), cur_list)
+            if not res:
+                res = queryPrice(dt, cur)
+            else:
+                res = res[0]
+            db_content.append(res)
+    db_content = sorted(db_content, key=lambda k: k['date'])
+    writePriceDatabase(db_content);
+
+if __name__ == "__main__":
+    main()
