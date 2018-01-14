0de4f3ac0c10a234ff51b7acd43769f1e115a8c9
indiedriver <amathias63@gmail.com>
2018-01-14 11:16:04 +0100

    Don't overwrite old elements

--- scripts/pricedb_update.py
+++ scripts/pricedb_update.py
@@ -40,7 +40,7 @@ def writePriceDatabase(list):
     with open(pricedb_path, "w") as file:
         for entry_list in list:
             file.write("P " + entry_list['date'].strftime("%Y/%m/%d %H:%M:%S") + " "
-                       + entry_list['symbol'] + " " + entry_list['price'] + fiat_symbol + "\n")
+                       + entry_list['symbol'] + " " + entry_list['price'] + entry_list['fiat_symbol'] + "\n")
 
 def queryPrice(dt, cur):
     price = getHistoricalPrice(cur, fiat_query, getTimestamp(dt));
@@ -48,12 +48,13 @@ def queryPrice(dt, cur):
         "date": dt,
         "symbol": cur,
         "price":  str(price),
+        "fiat_symbol": fiat_symbol
     }
     return (info)
 
 def main():
     content = []
-    parserExp = parse.compile("P {date} {time} {symbol} {value}" + fiat_symbol);
+    parserExp = parse.compile("P {date} {time} {symbol} {value:g}{fiat_symbol}");
     if os.path.exists(pricedb_path):
         with open(pricedb_path, "r") as file:
             for line in file:
@@ -63,6 +64,7 @@ def main():
                     "date": dt,
                     "symbol": res['symbol'],
                     "price":  str(res['value']),
+                    "fiat_symbol": res['fiat_symbol']
                 }
                 content.append(info)
     db_content = []
@@ -77,8 +79,11 @@ def main():
                 res = queryPrice(dt, cur)
             else:
                 res = res[0]
+                content.remove(res)
             db_content.append(res)
-    db_content = sorted(db_content, key=lambda k: k['date'])
+    for element in content:
+        db_content.append(element); # Push remaining elements to final array
+    db_content = sorted(db_content, key=lambda k: (k['date'], k['symbol']))
     writePriceDatabase(db_content);
 
 if __name__ == "__main__":
