90176a9d45a396832dd85e6bd61bfde2d29b15d2
Arnaud Mathias <arndmathias@gmail.com>
2025-05-11 20:10:51 +0200

    zsh: add y function for yazi

--- .zsh/custom_functions.zsh
+++ .zsh/custom_functions.zsh
@@ -2,14 +2,18 @@ function dot_update() {
     cd ~/.dotfiles/;
     git pull;
     git submodule update --recursive --remote;
-    #update vim plug
-    rm -f ~/.vim/autoload/plug.vim;
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-	curl -fo .gdbinit https://raw.githubusercontent.com/gdbinit/Gdbinit/master/gdbinit;
-	cd -;
+    cd -;
 }
 
 function music_import() {
-	ssh -p 1337 arnaud@192.168.0.2 -t 'cd /volume1/music/Import; beet import .;bash -l';
+    ssh -p 1337 arnaud@192.168.0.2 -t 'cd /volume1/music/Import; beet import .;bash -l';
+}
+
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
 }
